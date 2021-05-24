8a92ae9f7bbc30f3750201e60779892acffb761d
sowmen <sowmendipta@gmail.com>
2021-05-24 17:56:03 +0600

    segment train myv2

--- train_segment.py
+++ train_segment.py
@@ -28,8 +28,8 @@ from sim_dataset import SimDataset
 from segmentation.merged_netv2 import Mani_FeatX
 from segmentation.unetpp_v2 import MyUnetPP
 
-OUTPUT_DIR = "/content/drive/MyDrive/Image_Manipulation_Dataset/weights"
-CKPT_DIR = "/content/drive/MyDrive/Image_Manipulation_Dataset/checkpoint"
+OUTPUT_DIR = "weights"
+CKPT_DIR = "checkpoint"
 device = 'cuda'
 config_defaults = {
     "epochs": 60,
@@ -39,7 +39,7 @@ config_defaults = {
     "learning_rate": 0.0001,
     "weight_decay": 0.0005,
     "schedule_patience": 5,
-    "schedule_factor": 0.25,
+    "schedule_factor": 0.15,
     'sampling':'nearest',
     "model": "MyUnetPP-v2-Attn",
 }
@@ -464,7 +464,7 @@ from losses import DiceLoss, ImanipLoss
 def get_lossfn():
     bce = nn.BCEWithLogitsLoss()
     dice = DiceLoss(mode='binary', log_loss=True, smooth=1e-7)
-    criterion = ImanipLoss(bce, dice)
+    criterion = ImanipLoss(bce, dice, dice_weight=1.15)
     return criterion
 
     
@@ -518,7 +518,7 @@ if __name__ == "__main__":
 
     
     train(
-        name=f"(CASIA_FULL+customloss)" + config_defaults["model"],
+        name=f"(CASIA_FULL+sumloss1.15)" + config_defaults["model"],
         df=df,
         VAL_FOLD=0,
         resume=None,
