d246795e83b796c4619793387c08680f9806ad85
sowmen <sowmendipta@gmail.com>
2021-05-28 12:45:58 +0600

    lossing focal dice

--- install_requirements.sh
+++ install_requirements.sh
@@ -3,5 +3,5 @@ pip install -U git+https://github.com/albumentations-team/albumentations
 pip install git+https://github.com/rwightman/pytorch-image-models.git
 pip install git+https://github.com/qubvel/segmentation_models.pytorch.git
 pip install git+https://github.com/BloodAxe/pytorch-toolbelt.git
-pip install wandb
+pip install wandb==0.10.30
 pip install git+https://github.com/ildoonet/pytorch-gradual-warmup-lr.git
